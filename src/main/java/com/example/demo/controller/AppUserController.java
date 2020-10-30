33ccdf28911e1111a28512e323ce30ebf3c99aac
Nguyen Lam <nguyenduclam196@gmail.com>
2020-10-30 17:32:14 +0700

    hoàn thiện trang home, trang timeline, demo post. phải hoàn thiện thêm

--- src/main/java/com/example/demo/controller/AppUserController.java
+++ src/main/java/com/example/demo/controller/AppUserController.java
@@ -1,13 +1,18 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.AppUser;
+import com.example.demo.model.Post;
 import com.example.demo.service.appUserService.AppUserService;
+import com.example.demo.service.postService.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
@@ -16,10 +21,37 @@ public class AppUserController {
     @Autowired
     private AppUserService appUserService;
 
-    @GetMapping("")
+    @Autowired
+    private PostService postService;
+
+    @ModelAttribute("post")
+    public Post newPost(){
+        Post post = new Post();
+        return post;
+    }
+
+    @ModelAttribute("listPost")
+    public Iterable<Post> listPost(){
+        Iterable<Post> listPost= postService.findAll();
+        return listPost;
+    }
+
+    @ModelAttribute("user")
+    public AppUser currenUser(){
+        return appUserService.getCurrentUser();
+    }
+
+    @PostMapping("/creat/post")
+    public ModelAndView homePost(@ModelAttribute("post") Post post){
+        post.setAppUser(appUserService.getCurrentUser());
+        postService.save(post);
+        ModelAndView modelAndView = new ModelAndView("redirect:/users");
+        return modelAndView;
+    }
+
+    @GetMapping()
     public ModelAndView home(@ModelAttribute String username){
         ModelAndView modelAndView = new ModelAndView("home");
-        modelAndView.addObject("user", appUserService.getCurrentUser());
         return modelAndView;
     }
 
@@ -33,4 +65,8 @@ public class AppUserController {
         return "login";
     }
 
+    @GetMapping("/trangchu")
+    public String trangchu(){
+        return "timeline";
+    }
 }
